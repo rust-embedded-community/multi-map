2a374dd78d1e50ae51ffc84ee10d5706c0f3b2fe
theJPster <github@thejpster.org.uk>
2016-08-01 16:52:56 +0100

    Added documentation from 1.0.1

---
+++ doc/search-index.js
@@ -0,0 +1,3 @@
+var searchIndex = {};
+searchIndex["multi_map"] = {"doc":"# multi-map\nMultiMap is a HashMap that takes two different keys.","items":[[3,"MultiMap","multi_map","",null,null],[11,"new","","",0,{"inputs":[],"output":{"name":"multimap"}}],[11,"insert","","",0,null],[11,"get","","",0,null],[11,"get_mut","","",0,null],[11,"get_alt","","",0,null],[11,"get_mut_alt","","",0,null],[11,"remove","","",0,null],[11,"remove_alt","","",0,null],[11,"iter","","",0,null]],"paths":[[3,"MultiMap"]]};
+initSearch(searchIndex);
